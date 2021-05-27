6a88b5cf80719d5019058266afa2523eda2e9ea9
Centeno448 <diego.centeno@udla.edu.ec>
2021-05-27 12:17:06 -0500

    udpates messages

--- src/App.test.js
+++ src/App.test.js
@@ -4,10 +4,11 @@ import { shallow } from 'enzyme';
 import App from "./App";
 import TareasLista from "./TareasLista"
 
-configure({ adapter: new Adapter() });
+beforeAll(() => {
+  configure({ adapter: new Adapter() });
+});
 
-test('App.js tiene componente hijo TareasLista.js - Asegurate de renderizar el componente <TareasLista> dentro de App.js', () => {
+test('App.js tiene 1 componente hijo TareasLista.js - Asegurate de renderizar 1 vez el componente <TareasLista> dentro de App.js', () => {
   const wrapper = shallow(<App />);
   expect(wrapper.find(TareasLista).length).toBe(1);
 });
-
