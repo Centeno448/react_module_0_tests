1305a4f649ac43988f8553d0cbe763767d6fc361
Centeno448 <diego.centeno@udla.edu.ec>
2021-05-28 19:53:27 -0500

    fixed messages

--- src/App.test.js
+++ src/App.test.js
@@ -8,7 +8,7 @@ beforeAll(() => {
   configure({ adapter: new Adapter() });
 });
 
-test('App.js tiene 1 componente hijo TareasLista.js - Asegurate de renderizar 1 vez el componente <TareasLista> dentro de App.js', () => {
+test('App.js tiene 1 componente hijo TareasLista.js | Asegúrate de renderizar 1 vez el componente <TareasLista> dentro de App.js', () => {
   const wrapper = shallow(<App />);
   expect(wrapper.find(TareasLista).length).toBe(1);
 });
